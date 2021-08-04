0acc2d6ca1b6df73ee87b8c5f14c7b1369d41fef
Md. Mostafizur Rahman <38970298+mostafiz9900@users.noreply.github.com>
2021-08-04 11:17:24 +0600

    sfs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,21 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-import salesRoute from './sales.route';
-import userRoute from './user.route';
+import salesRoute from "./sales.route";
+import userRoute from "./user.route";
 /**
  * Function contains Application routes
  *
  * @returns router
  */
 const routes = () => {
-  router.get('/', (req, res) => {
-    res.json('Welcome');
+  router.get("/", (req, res) => {
+    res.json("Welcome");
   });
-  router.use('/users', userRoute);
+  // eslint-disable-next-line quotes
+  router.use("/users", userRoute);
 
-  router.use('/sales', salesRoute);
+  router.use("/sales", salesRoute);
   return router;
 };
 
